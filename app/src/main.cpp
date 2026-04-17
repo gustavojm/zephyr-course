fa67a8a35ce37d0868fac62e85c2ac7557a84a00
Gustavo Malano <gmalano@na-sa.com.ar>
2026-04-17 15:12:34 -0300

    working

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,7 +5,7 @@
 #define SLEEP_TIME_MS 250
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+#define LED_NODE DT_ALIAS(status_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
