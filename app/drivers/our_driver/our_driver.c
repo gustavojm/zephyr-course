70bc60899aa3c04e7e1a3c8d35977542e4271b7f
Gustavo Malano <gmalano@na-sa.com.ar>
2026-05-07 10:43:47 -0300

    using log system instead of direct printk

--- app/drivers/our_driver/our_driver.c
+++ app/drivers/our_driver/our_driver.c
@@ -1,36 +1,43 @@
 #include "zephyr/devicetree.h"
+#include "zephyr/logging/log_core.h"
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
 #define DT_DRV_COMPAT our_driver
 
-int channel_get_my_impl(const struct device *dev,
-				    enum sensor_channel chan,
-				    struct sensor_value *val) {
+LOG_MODULE_REGISTER(
+    our_driver, LOG_LEVEL_INF); // Must register ONCE, Other files must call
+                                // LOG_MODULE_DECLARE(our_module, LOG_LEVEL_INF)
 
-    return (int)chan;
+int channel_get_my_impl(const struct device *dev, enum sensor_channel chan,
+                        struct sensor_value *val) {
+
+  LOG_INF("channel_get_impl called for chan %d", chan);
+
+  return 42;
 }
 
-int init_function(const struct device* dev) {
-   // printk("driver initialized");
-    return 0;
+int init_function(const struct device *dev) {
+  // printk("driver initialized");
+  LOG_INF("our_driver Initialized");
+  return 0;
 }
 
-static DEVICE_API(sensor, api_led_driver) = {
-    .channel_get = channel_get_my_impl
-};
+static DEVICE_API(sensor,
+                  api_led_driver) = {.channel_get = channel_get_my_impl};
 
-/* This creates one instance of the driver, if more than one is enabled in the DTS this line should be repeated for the following instance, 
- * the first parameter is the instance number
- * DEVICE_DT_INST_DEFINE(0, init_function, NULL, NULL, NULL, POST_KERNEL, 80, &api_led_driver);
- * so instead of repeating this we do:
+/* This creates one instance of the driver, if more than one is enabled in the
+ * DTS this line should be repeated for the following instance, the first
+ * parameter is the instance number DEVICE_DT_INST_DEFINE(0, init_function,
+ * NULL, NULL, NULL, POST_KERNEL, 80, &api_led_driver); so instead of repeating
+ * this we do:
  */
 
- #define DEV_INST(inst) DEVICE_DT_INST_DEFINE(inst, init_function, NULL, NULL, NULL, POST_KERNEL, 80, &api_led_driver);
+#define DEV_INST(inst)                                                         \
+  DEVICE_DT_INST_DEFINE(inst, init_function, NULL, NULL, NULL, POST_KERNEL,    \
+                        80, &api_led_driver);
 
- DT_INST_FOREACH_STATUS_OKAY(DEV_INST)          // It uses DT_DRV_COMPAT macro to filter the DTS searching for nodes with compatible = "our,driver"
-
-
-
-
+DT_INST_FOREACH_STATUS_OKAY(
+    DEV_INST) // It uses DT_DRV_COMPAT macro to filter the DTS searching for
+              // nodes with compatible = "our,driver"
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,44 +1,45 @@
 // #include "syscalls/sensor.h"
 #include "zephyr/drivers/sensor.h"
+#include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/device.h>
 
 /* The devicetree node identifier for the "led0" alias. */
 // #define LED_NODE DT_ALIAS(status_led)
 
 // static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-static const struct device *our_driver0 = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
-static const struct device *our_driver1 = DEVICE_DT_GET(DT_NODELABEL(our_driver1));
-
+static const struct device *our_driver0 =
+    DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+static const struct device *our_driver1 =
+    DEVICE_DT_GET(DT_NODELABEL(our_driver1));
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-int main(void)
-{   
-    struct sensor_value val;
-     // bool led_state = true;
+int main(void) {
+  struct sensor_value val;
+  // bool led_state = true;
 
-    // if (!gpio_is_ready_dt(&led)) return 0;
+  // if (!gpio_is_ready_dt(&led)) return 0;
 
-    // if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+  // if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
-    while (1) {
-    auto ret = sensor_channel_get(our_driver0, SENSOR_CHAN_AMBIENT_TEMP,  &val);
+  while (1) {
+    auto ret = sensor_channel_get(our_driver0, SENSOR_CHAN_AMBIENT_TEMP, &val);
 
     printk("sensor read from main %i \n", ret);
 
-    ret = sensor_channel_get(our_driver1, SENSOR_CHAN_ALTITUDE,  &val);
+    ret = sensor_channel_get(our_driver1, SENSOR_CHAN_ALTITUDE, &val);
 
     printk("sensor read from main sensor_1 %i \n", ret);
 
-  //     if (gpio_pin_toggle_dt(&led) < 0) return 0;
+    //     if (gpio_pin_toggle_dt(&led) < 0) return 0;
 
     //     led_state = !led_state;
     //     LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-    //     k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
-    }
-    return 0;
+    k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+  
+  }
+  return 0;
 }
