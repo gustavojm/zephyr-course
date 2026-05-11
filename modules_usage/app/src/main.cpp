e1945ec804f79df6dfcd6b99702c29a9cfdc8dd6
Gustavo Malano <gmalano@na-sa.com.ar>
2026-05-11 12:48:40 -0300

    working using method ZEPHYR_EXTRA_MODULES

--- modules_usage/app/src/main.cpp
+++ modules_usage/app/src/main.cpp
@@ -5,7 +5,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#include <app/drivers/our_driver.h>
+#include <drivers/our_driver/our_driver.h>
 
 /* The devicetree node identifier for the "led0" alias. */
 // #define LED_NODE DT_ALIAS(status_led)
