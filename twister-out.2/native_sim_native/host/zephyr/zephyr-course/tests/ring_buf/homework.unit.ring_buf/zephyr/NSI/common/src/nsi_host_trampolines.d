ec058f0b4cc7c90cecfbeff9bda44b148b57e0d5
Gustavo Malano <gmalano@na-sa.com.ar>
2026-05-19 14:50:08 -0300

    removed twister outputs from repo

--- twister-out.2/native_sim_native/host/zephyr/zephyr-course/tests/ring_buf/homework.unit.ring_buf/zephyr/NSI/common/src/nsi_host_trampolines.d
+++
@@ -1,2 +0,0 @@
-/home/gustavo/zephyr/zephyr-course/twister-out/native_sim_native/host/zephyr/zephyr-course/tests/ring_buf/homework.unit.ring_buf/zephyr/NSI/common/src/nsi_host_trampolines.o: \
- /home/gustavo/zephyrproject/zephyr/scripts/native_simulator//common/src/nsi_host_trampolines.c
--- twister-out.5/native_sim_native/host/zephyr/zephyr-course/tests/ring_buf/homework.unit.ring_buf/zephyr/NSI/linker_script.d
+++
@@ -1,2 +0,0 @@
-linker_script.pre.o: \
- /home/gustavo/zephyrproject/zephyr/scripts/native_simulator//common/other/linker_script.pre.ld
